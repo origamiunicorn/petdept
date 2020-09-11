154e7aaf088dc957c71ba767574648d29e392d58
origami unicorn <rvwright@gmail.com>
2020-09-11 15:27:24 -0700

    Setup basic top nav, with basic page structure, set up basic sidenav.

---
+++ assets/js/nav.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sidenavs = document.querySelectorAll('.sidenav')
+    for (let i = 0; i < sidenavs.length; i++) {
+        M.Sidenav.init(sidenavs[i]);
+    }
+    const dropdowns = document.querySelectorAll('.dropdown-trigger')
+    for (let i = 0; i < dropdowns.length; i++) {
+        M.Dropdown.init(dropdowns[i]);
+    }
+    const collapsibles = document.querySelectorAll('.collapsible')
+    for (let i = 0; i < collapsibles.length; i++) {
+        M.Collapsible.init(collapsibles[i]);
+    }
+    const featureDiscoveries = document.querySelectorAll('.tap-target')
+    for (let i = 0; i < featureDiscoveries.length; i++) {
+        M.FeatureDiscovery.init(featureDiscoveries[i]);
+    }
+    const materialboxes = document.querySelectorAll('.materialboxed')
+    for (let i = 0; i < materialboxes.length; i++) {
+        M.Materialbox.init(materialboxes[i]);
+    }
+    const modals = document.querySelectorAll('.modal')
+    for (let i = 0; i < modals.length; i++) {
+        M.Modal.init(modals[i]);
+    }
+    const parallax = document.querySelectorAll('.parallax')
+    for (let i = 0; i < parallax.length; i++) {
+        M.Parallax.init(parallax[i]);
+    }
+    const scrollspies = document.querySelectorAll('.scrollspy')
+    for (let i = 0; i < scrollspies.length; i++) {
+        M.ScrollSpy.init(scrollspies[i]);
+    }
+    const tabs = document.querySelectorAll('.tabs')
+    for (let i = 0; i < tabs.length; i++) {
+        M.Tabs.init(tabs[i]);
+    }
+    const tooltips = document.querySelectorAll('.tooltipped')
+    for (let i = 0; i < tooltips.length; i++) {
+        M.Tooltip.init(tooltips[i]);
+    }
+});
